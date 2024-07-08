428ca7982a2390eb161b6060af4a66e4dbe856db
earth2travis <travis.mccutcheon@gmail.com>
2024-07-08 12:12:38 -0500

    Add FrogUI

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 ```
-npm install
-npm run dev
+pnpm install
+pnpm run dev
 ```
 
 Head to http://localhost:5173/api
+
+```bash
+pnpm create frog -t vercel
+```
+
+```bash
+pnpm update
+```
+
+Deploy to Vercel:
+
+https://vercel-serverless-swart-two.vercel.app/
+
+Check in Frame Validator:
+
+https://warpcast.com/~/developers/frames?url=https%3A%2F%2Fvercel-serverless-swart-two.vercel.app%2Fapi
+
+Add FrogUI
--- api/index.tsx
+++ api/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Frog, TextInput } from 'frog';
+import { Frog } from 'frog';
+import { Box, Heading, Text, VStack } from './ui.js';
 import { devtools } from 'frog/dev';
 import { serveStatic } from 'frog/serve-static';
 import { handle } from 'frog/vercel';
@@ -10,52 +11,22 @@ export const app = new Frog({
 });
 
 app.frame('/', c => {
-  const { buttonValue, inputText, status } = c;
-  const fruit = inputText || buttonValue;
   return c.res({
     image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
+      <Box
+        grow
+        alignHorizontal="center"
+        backgroundColor="background"
+        padding="32"
       >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
+        <VStack gap="4">
+          <Heading>FrogUI 🐸</Heading>
+          <Text color="text200" size="20">
+            Build consistent frame experiences
+          </Text>
+        </VStack>
+      </Box>
     ),
-    intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
   });
 });
 
